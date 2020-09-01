dad0f2700f054af263e762ee8a4249799b8e5a1e
Ty Gavin <ty@tealium.com>
2020-08-31 17:14:07 -0700

    initial add

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# tealium-collect-adobe-launch-extension
+# Tealium Collect Extension for Adobe Launch
+
+## Overview
+
+This is the Adobe Launch Extension for Tealium Collect data collection.
+
+The Tealium Collect Extension for Adobe Launch allows you to enable and configure data collection for Tealium's Customer Data Hub (CDH) on your website using Adobe Launch.
+
+For more information on Tealium's CDH, please visit [Tealium's Learning Community](https://community.tealiumiq.com/t5/Customer-Data-Hub/Introduction-to-Customer-Data-Hub/ta-p/17571)
+
+## Quickstart Configuration
+
+1. Search for the "Tealium Collect" Extension in the Adobe Extension Catalog
+2. Click Install button to add this Extension
+3. Add a Rule and Action for "Tealium Collect"
+4. Enter the Tealium Account, Profile, and Data Source Key obtained from Tealium's CDH
+5. Publish
+
+
+## Features
+
+
+1. Custom Endpoint
+
+Configure with your first-party data collection endpoint.  Include the "/event" part of the URL.
+
+2. Custom Data Object
+
+This is a name of a global object to use for the data layer.  By default, the Tealium Collect tag template uses the global 'digitalData' object as the data layer. 
+
+3. Support for Direct Call Rule with \_satellite.track
+
+This Extension also supports tracking events that were initiated via [\_satellite.track](https://docs.adobe.com/content/help/en/launch/using/reference/client-side-info/launch-object-reference.html) function call.  The first param will be the event name and the second param will be used for the data layer.  The Tealium Collect Extension also collects the \_satellite.buildInfo information and adds to the data layer.
+
+## Copyright and license
+
+Copyright 2020 Tealium, Inc. All rights reserved.
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
---
+++ src/lib/actions/track.js
@@ -0,0 +1,96 @@
+'use strict';
+var window = require('@adobe/reactor-window');
+var loadScript = require('@adobe/reactor-load-script');
+var extensionSettings = turbine.getExtensionSettings();
+
+module.exports = function(settings, event) {
+  var eventData = {},
+    eventName = event["$rule"].name;
+
+  turbine.logger.log('Tealium Collect settings: ( ' + JSON.stringify(extensionSettings) + ')');
+   
+  eventData.dataObject = window[extensionSettings.dataObjectName] || {};
+
+  // For a "Direct Call" action using _satellite.track, use event.detail for the data layer
+  if (event["$type"] === "core.direct-call") {
+     eventData.dataObject = event.detail;
+  }
+  /*
+     _satellite.track("direct", {hello:"direct_world"});
+
+     event = {
+       "identifier": "direct",
+       "detail": {
+           "hello": "direct_world"
+       },
+       "$type": "core.direct-call",
+       "$rule": {
+           "id": "RL024474b1bce14a04ac3ebd745a83176e",
+           "name": "Direct"
+       }
+     }
+  */
+
+  // Create global object before loading tealium.js
+  window.tealium = window.tealium || {q:[]};
+
+  if (window.tealium.track === undefined) {
+    // Waiting for tealium.js library to finish loading
+    turbine.logger.log('Tealium Collect queuing tracking event: ' + eventName);
+    window.tealium.q.push(["track", eventName, eventData]);
+  } else {
+    // No need to queue events when the library has loaded
+    turbine.logger.log('Tealium Collect tracking event: ' + eventName);
+    window.tealium.track(eventName, eventData);
+  }
+
+  // This code will only run once
+  if (window.Tealium === undefined) {
+    window.Tealium = {};
+    turbine.logger.log('Tealium Collect library loading...');
+
+    // Both Tealium function and tealium.track function created inside Tealium Collect JS library
+    loadScript('https://tags.tiqcdn.com/libs/tealiumjs/latest/tealium_collect.min.js').then(function(){
+      var config = [];
+
+      turbine.logger.log('Tealium Collect library loading complete.');
+      config.push(["config", "init", [extensionSettings.account, extensionSettings.profile, "prod", extensionSettings.dataSourceKey]]);
+      
+      if (window._satellite && window._satellite.buildInfo && window._satellite.buildInfo.environment === "development") {
+        config.push(["config", "debug", true]);
+      }
+
+      // This will flatten the data layer to prepare for server-side mapping
+      config.push(["config", "functions", [{
+        id: 1,
+        config: {
+          before_rules: true  
+        },
+        f: function (tag, d, t, w) {
+          t.debug(d);
+          // Automatically grab some interesting data from _satellite.buildInfo object
+          // buildDate, environment, turbineBuildDate, turbineVersion
+          if (window._satellite && _satellite.buildInfo) {
+            t.util.merge(d, _satellite.buildInfo);
+          }
+          // Only goes 5 levels deep, non-flattened items will still be references to original
+          // Two rounds of "flatten" to help with W3C digitalData arrays of multi-level objects
+          t.util.merge(d, t.util.flatten(t.util.flatten(d.dataObject, 5, true), 5, true));
+  
+          // Remove reference to original object
+          d.dataObject = extensionSettings.dataObjectName;
+          t.debug(d);
+                  
+          return true;
+        }
+      }]]);
+
+      config.push(["config", "addTag", {name: "tealium_collect", version: "1.0.3", server: extensionSettings.endpoint}]);
+
+      // Process queue of events and create new global tealium object with tealium.track function defined
+      window.tealium = window.Tealium(config.concat(window.tealium.q));
+    });
+  }
+ 
+};
+
